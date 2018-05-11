aee9802316b72f4967bf478c2548bfe638f00e03
junwangustc <junwangustc@gmail.com>
2018-05-11 18:12:50 +0800

    Update licode_config.js
    change supervisID

--- licode_config.js
+++ licode_config.js
@@ -23,10 +23,10 @@ config.cloudProvider.name = '';
  NUVE CONFIGURATION
 **********************************************************/
 config.nuve = {};
-config.nuve.dataBaseURL = "10.8.126.84:27017/localnuvedb"; // default value: 'localhost/nuvedb'
-config.nuve.superserviceID = '123456789'; // default value: ''
-config.nuve.superserviceKey = '123456789'; // default value: ''
-config.nuve.testErizoController = 'localhost:8080'; // default value: 'localhost:8080'
+config.nuve.dataBaseURL = "10.8.126.84:27017/nuvedb"; // default value: 'localhost/nuvedb'
+config.nuve.superserviceID = '56c2db5db92de8c63ff53'; // default value: ''
+config.nuve.superserviceKey = '4802'; // default value: ''
+config.nuve.testErizoController = '10.8.126.84:8080'; // default value: 'localhost:8080'
 // Nuve Cloud Handler policies are in nuve/nuveAPI/ch_policies/ folder
 config.nuve.cloudHandlerPolicy = 'default_policy.js'; // default value: 'default_policy.js'
 
